1ce8d6a1e0648c86ecf53401caff32954286b3d4
PederBHelland <peder.helland@hotmail.com>
2017-09-28 15:47:17 +0200

    Til Jinci

--- Project2/code/2b/main.cpp
+++ Project2/code/2b/main.cpp
@@ -93,21 +93,29 @@ vec eigenvalues(mat A, mat B, int N, int k, int l, double tau, double t, double 
     return eig;
 }
 
+vec eigenvectors(mat A, mat B, int N, int k, int l, double tau, double t, double c, double s, int i, int counter_zero_elements, int number_of_elements_over_diagonal, int u, int v, mat A_original, double eps, vec eig, vec eig_vec)
+{
+    A = Jacobi_rotation(A, B, N, k, l, tau, t, c, s, i, counter_zero_elements, number_of_elements_over_diagonal, u, v, A_original, eps);
+//for(int i=0; i <N ; i++){
+    if (N== 3){
+        eig_vec = {-(A(0,2)*A(1,0) + A(1,2)*(eig(0) - A(0,0)))/(A(0,1)*A(1,0) + (A(1,1)-eig(0))*(eig(0)-A(0,0))),A(0,2)/(eig(0) -A(0,0))-(A(0,1)*(A(0,2)*A(1,0) + A(1,2)*(eig(0) - A(0,0))))/((eig(0)-A(0,0))*(A(0,1)*A(1,0) + (A(1,1)-eig(0))*(eig(0)-A(0,0)))), 1};
+    }
+    //}
+    return eig_vec;
+}
 
-vec setup(int N, int M, double rho_max, double eps, string potential) {
+vec setup(int N, int M, double rho_max, double eps, string potential, double w_r=0) {
     int i, j, k, l, x, counter_zero_elements, u, v;
     int number_of_elements_over_diagonal;
     //int  N=4, M=100;
     double tau;
     double theta;
     double rho_min = 0;
-    //double rho_max = 5;
-    //double eps = 0.0000001;
 
     mat A = zeros<mat>(N,N);
     mat B = zeros<mat>(N,N);
     mat A_original = zeros<mat>(N,N);
-    vec rho(N), V(N), eig(N), omega_r;
+    vec rho(N), V(N), eig(N), eig_vec(N);
 
     double t, c, s; //tan, cos, sin
 
@@ -115,10 +123,12 @@ vec setup(int N, int M, double rho_max, double eps, string potential) {
 
     for(i = 1; i<N+1; i++){
         rho(i-1) = rho_min + i*h;
-        if(potential == ""){
-
+        if(potential == "harmonic"){
+            V(i-1) = rho(i-1)*rho(i-1);
         }
-        V(i-1) = rho(i-1)*rho(i-1);
+        if(potential == "coulomb"){
+            V(i-1) = w_r*w_r*rho(i-1)*rho(i-1) + 1./rho(i-1);
+        }
     }
 
     for(i = 1; i< N-1; i++){
@@ -134,49 +144,32 @@ vec setup(int N, int M, double rho_max, double eps, string potential) {
 
     B=A;
     eig = eigenvalues(A, B, N, k, l, tau, t, c, s, i, counter_zero_elements, number_of_elements_over_diagonal, u, v, A_original, eps);
-    cout << " \n \n" << eig;
-
-    double beta_e_2 = 1.44;
-    double h_bar = 1;
-    double m = 1;
-    double alfa = (h_bar*h_bar)/(m*beta_e_2);
-    vec rho_2(N);
-
-    omega_r = {0.01, 0.5, 1, 5};
-
+    eig_vec = eigenvectors(A, B, N, k, l, tau, t, c, s, i, counter_zero_elements, number_of_elements_over_diagonal, u, v, A_original, eps, eig, eig_vec);
+    cout << eig_vec;
     return eig;
-
-    //cout << omega_r;
-    /*for(i = 1; i < N; i++){
-    rho_2 = (omega_r*omega_r)*(rho(i)*rho(i)) + 1/rho(i);
-}*/
-    //cout << rho_2;
-
-
-
-
-    /*//Print to output file
-const char *path="/Documents/FYS3150/FYS4150/Project2/code/results.txt";
-ofstream myfile(path);
-myfile << n << "\n";
-for(i=0; i<n; i++){
-    myfile << x[i] << " " << u[i] << " " << v[i] << "\n";
 }
-myfile.close();*/
-}
+
 
 int main(int argc, char* argv[]) {
     int dumb = 0;
     char* dumb1[1];
     int result = Catch::Session().run(dumb, dumb1);
-    vec eig = setup(4, 100, 5, 1e-7);
+
+    //Oppgave d
+    double w_r = 0.01;
+    string potential = "coulomb";
+    vec eig = setup(3, 100, 5, 1e-7, potential, w_r);
+    //if(potential == "coulomb"){
+    //    cout << "The eigenvalues in the interaction case are:" << endl << eig << endl;
+    //}
+
 }
 
 TEST_CASE( "Eigenvalues are correct", "[eigenvalues]" ) {
-    REQUIRE(fabs(setup(4, 100, 5, 1e-7)(0) - 2.7561) < 1e-4);
-    REQUIRE(fabs(setup(4, 100, 5, 1e-7)(1) - 7.5639) < 1e-4);
-    REQUIRE(fabs(setup(4, 100, 5, 1e-7)(2) - 15.3575) < 1e-4);
-    REQUIRE(fabs(setup(4, 100, 5, 1e-7)(3) - 26.3174) < 1e-4);
+    REQUIRE(fabs(setup(4, 100, 5, 1e-7, "harmonic")(0) - 2.7561) < 1e-4);
+    REQUIRE(fabs(setup(4, 100, 5, 1e-7, "harmonic")(1) - 7.5639) < 1e-4);
+    REQUIRE(fabs(setup(4, 100, 5, 1e-7, "harmonic")(2) - 15.3575) < 1e-4);
+    REQUIRE(fabs(setup(4, 100, 5, 1e-7, "harmonic")(3) - 26.3174) < 1e-4);
 }
 
 
