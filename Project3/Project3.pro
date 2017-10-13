81f3603f8f5e056751f286000ec879b039e70cc0
Mona Anderssen <monande@spole.uio.no>
2017-10-13 15:16:03 +0200

    Euler with a mess

--- Project3/Project3.pro
+++ Project3/Project3.pro
@@ -4,3 +4,5 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp
+INCLUDEPATH += /usr/local/include
+LIBS += -L/usr/local/lib -larmadillo -llapack -lblas
--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include "armadillo"
+#include <math.h>
+#include <fstream>
+#include <chrono>
+#include "time.h"
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
+using namespace arma;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int N = 100;
+    vec a, v, x, t;
+    double dt = 1/N;
+    double M_earth = 6e24;
+    double M_sun = 2e30;
+    double G = 6.67408e-11;
+    double r = 1.5e11;
+//    double m = M_earth;
+    double F = (G*M_sun*M_earth)/(r*r);
+    for (int i = 0; i < N-1; i++){
+        a(i) = F/M_earth;
+        v(i+1) = v(i) + a(i+1)*dt;
+        x(i+1) = x(i)  + v(i+1)*dt;
+        t(i+1) = t(i) + dt;
+    }
+    cout << a << endl;
 }
